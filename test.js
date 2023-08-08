84a7834b9df34159bc60d35df6852dde44e7705a
svan9 <ivan.selyakovich@gmail.com>
2023-08-09 02:42:45 +0300

    1.0.08

--- test.js
+++ test.js
@@ -1,3 +1,3 @@
-uv.define("./components/button.uv", "./components/lot.uv");
+uv.define("./components/button.uv", "./components/lot.uv", "./components/spoiler.uv");
 // uv.define("./components/lot.uv", "./components/button.uv");
 uv.render();
